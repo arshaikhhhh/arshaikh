9d63021d94660cd3e8e034751a58eb9158b26c45
Abdul Rahim Shaikh <93668676+BlackArch0@users.noreply.github.com>
2025-05-28 17:03:13 +0000

    commetnted code

--- admin.js
+++ admin.js
@@ -1,26 +1,34 @@
-// // console.log('ar');
-// // console.log('first file puss')
+// // // console.log('ar');
+// // // console.log('first file puss')
 
-// // let a = [1, 2, 3, 4, 5];
-// // let b = a.slice(0, 3);
-// // console.log(b);
-// // console.log('second file push');
-// // console.log (a);
+// // // let a = [1, 2, 3, 4, 5];
+// // // let b = a.slice(0, 3);
+// // // console.log(b);
+// // // console.log('second file push');
+// // // console.log (a);
 
-// let arr = [1, 2, 3, 4, 5];
-// let arr2 = arr.splice(0, 3);
-// console.log(arr2);
-// console.log('third file push');
-// console.log(arr);
-// console.log('fourth file push');
+// // let arr = [1, 2, 3, 4, 5];
+// // let arr2 = arr.splice(0, 3);
+// // console.log(arr2);
+// // console.log('third file push');
+// // console.log(arr);
+// // console.log('fourth file push');
 
-let a = {
-    name: 'John',
-    age: 30,
-    city: 'New York'
-}
+// let sym = Symbol('description');
 
-a.usergreet = function lol(){
-   console.log('owfjoe')
-}
-console.log(a.usergreet())
+// let a = {
+//     name: 'John',
+//     age: 30,
+//     [sym]: 'This is a symbol property',
+//     city: 'New York'
+// }
+// console.log(a)
+// // Object.freeze(a); // Freezes the object, preventing any changes to its properties
+// console.log(a[sym] = "chut marina");
+// console.log(a)
+
+// // a.usergreet = function lol(){
+// //    console.log('owfjoe')
+// // }
+// // console.log(a.usergreet()) 
+
