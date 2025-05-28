e091071662cef9508e3ecd70d3075ad96fbdf8b4
Abdul Rahim Shaikh <93668676+BlackArch0@users.noreply.github.com>
2025-05-28 14:47:47 +0000

    arshaikh

--- admin.js
+++ admin.js
@@ -1,2 +1,15 @@
-console.log('ar');
-console.log('first file puss')
+// console.log('ar');
+// console.log('first file puss')
+
+// let a = [1, 2, 3, 4, 5];
+// let b = a.slice(0, 3);
+// console.log(b);
+// console.log('second file push');
+// console.log (a);
+
+let arr = [1, 2, 3, 4, 5];
+let arr2 = arr.splice(0, 3);
+console.log(arr2);
+console.log('third file push');
+console.log(arr);
+console.log('fourth file push');
