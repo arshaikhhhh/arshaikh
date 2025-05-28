98b0b4d979383c7deaf87fcd6bcd0f922beb885e
Abdul Rahim Shaikh <93668676+BlackArch0@users.noreply.github.com>
2025-05-28 16:50:05 +0000

    leraning lodu

--- admin.js
+++ admin.js
@@ -1,15 +1,26 @@
-// console.log('ar');
-// console.log('first file puss')
+// // console.log('ar');
+// // console.log('first file puss')
 
-// let a = [1, 2, 3, 4, 5];
-// let b = a.slice(0, 3);
-// console.log(b);
-// console.log('second file push');
-// console.log (a);
+// // let a = [1, 2, 3, 4, 5];
+// // let b = a.slice(0, 3);
+// // console.log(b);
+// // console.log('second file push');
+// // console.log (a);
 
-let arr = [1, 2, 3, 4, 5];
-let arr2 = arr.splice(0, 3);
-console.log(arr2);
-console.log('third file push');
-console.log(arr);
-console.log('fourth file push');
+// let arr = [1, 2, 3, 4, 5];
+// let arr2 = arr.splice(0, 3);
+// console.log(arr2);
+// console.log('third file push');
+// console.log(arr);
+// console.log('fourth file push');
+
+let a = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+}
+
+a.usergreet = function lol(){
+   console.log('owfjoe')
+}
+console.log(a.usergreet())
