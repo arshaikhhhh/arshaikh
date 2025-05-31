7525e902b7aafd1bfb780a99117e3dfb27206480
Abdul Rahim Shaikh <93668676+BlackArch0@users.noreply.github.com>
2025-05-31 18:59:29 +0000

    switch-case

--- admin.js
+++ admin.js
@@ -316,3 +316,32 @@
 //     console.log("here we are");
     
 // }
+
+// let month = 6;
+
+// switch (month){
+//     case 1:
+//         console.log("jan");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+//     case 10:
+//         console.log("june");
+//         break;
+//     case 7:
+//         console.log("july");
+//         break;
+//     default :
+//     console.log("the month does'nt exist");
+//     break;  
+// }
