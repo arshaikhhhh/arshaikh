036764f9b7dd6fca7ef49d374e10d7761fac6720
BlackArch0 <shaikhar287@gmail.com>
2025-06-11 19:49:54 +0530

    Keyboard Key Detection

--- admin.js
+++ admin.js
@@ -960,7 +960,23 @@
 //   clearInterval(inter)
 //   inter = null;
 // })
-
+let insert = document.querySelector('.keyin');
+window.addEventListener('keydown', function(e){
+    insert.innerHTML = `
+    <table>
+  <tr>
+    <td>key</td>
+    <td>keycode</td>
+    <td>code</td>
+  </tr>
+  <tr>
+    <td>${e.key === " "?"space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+    `
+})
 
 
 
